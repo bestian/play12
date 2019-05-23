720da7f7315da8f89b89347216eb3a4a8014c1ab
Bestian Tang 唐宗浩 <besitan@gmail.com>
2019-05-23 09:15:22 +0800

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  title : string = 'play12'
+  cards : [number] = [6, 4, 2, 1]
 
+  public reset () {
+    for (var i = 0; i < 4; ++i) {
+    	this.cards[i] = Math.floor(Math.random() * 6) + 1
+    }
+  }
 }
