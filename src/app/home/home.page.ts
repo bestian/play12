b264ef67af4e356a22015b586f8933d1827bb8c9
Bestian Tang <bestian@gmail.com>
2019-06-11 19:24:22 +0800

    css & text better

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  win: boolean = false;
+  wins : number = 0;
   myNum : number = 0;
   title : string = '加減乘除湊12';
   cards : [number, number, number, number] = [
@@ -14,7 +16,20 @@ export class HomePage {
   	Math.floor(Math.random() * 6) + 1,
   	Math.floor(Math.random() * 6) + 1
   ];
-  used : [boolean, boolean, boolean, boolean] = [false, false, false, false]
+
+  public iWin () {
+    this.win = true;
+    this.wins++
+  }
+
+  public isWin () {
+    var ans = true
+    for (var i = 0; i < this.cards.length; ++i) {
+      if (this.cards[i] != 0) { ans = false }
+    }
+    if (this.myNum != 12) { ans = false }
+    return ans
+  }
 
   public use (idx) {
   	this.cards[idx] = 0;
@@ -25,6 +40,7 @@ export class HomePage {
     	this.cards[i] = Math.floor(Math.random() * 6) + 1
     }
     this.myNum = 0;
+    this.win = false;
   }
 
   public makecard () {
@@ -55,5 +71,9 @@ export class HomePage {
   		default:
   			this.myNum = 0
   	}
+    console.log(this.isWin())
+    if (this.isWin()) {
+      this.iWin()
+    }
   }
 }
